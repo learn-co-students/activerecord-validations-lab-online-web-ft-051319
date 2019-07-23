c1fffe6562ff9b028ccc123a7579daa94065279c
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-07-23 17:26:51 +0800

    Done.

--- app/models/click_bait_validator.rb
+++ app/models/click_bait_validator.rb
@@ -7,12 +7,11 @@ class ClickBaitValidator < ActiveModel::Validator
   #   end
   # end
 
+  CLICKBAIT = [/Won't Believe/i,/Secret/i,/Top [0-9]*/i,/Guess/i]
+
   def validate(record)
-    required=["Won't Believe", "Secret", "Top", "Guess"]
-    if !record.title==""
-      unless required.any?{|word| record.title.include?(word)} #returns true at the first instance record.title contains an element from the required list
-        record.errors[:title] << "Not clickbaity enough"
-      end
+    unless CLICKBAIT.any?{|word| word.match record.title} #returns true at the first instance record.title contains an element from the required list
+      record.errors[:title] << "Not clickbaity enough"
     end
   end
 end
