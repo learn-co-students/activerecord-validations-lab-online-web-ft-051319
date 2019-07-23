16fc122286e1262375a87bd4c37f8e523995f4db
Brian <briandsalemi@gmail.com>
2019-07-23 10:59:10 -0400

    Done.

--- app/models/myvalidator.rb
+++
@@ -1,9 +0,0 @@
-class MyValidator < ActiveModel::Validator 
-    
-    def validate(post)
-        unless post.title.include?("Won't Believe") || title.include?("Secret") || title.include?("Guess") || title.include?("Top[number]")
-            return false 
-        end 
-    end 
-
-end 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,19 @@
 class Post < ActiveRecord::Base
-    include ActiveModel::Validations
-    validates_with MyValidator 
+    validate :title_is_click_baity, on: :create
 
     validates :title, presence: true 
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    def title_is_click_baity
+        # binding.pry
+        if self.title 
+            if self.title.include?("Won't Believe") || self.title.include?("Secret") || self.title.include?("Guess") || self.title.include?("Top[number]") 
+                true  
+            else 
+                errors.add(:title, "Not Clickbait")
+            end  
+        end 
+    end 
 end
