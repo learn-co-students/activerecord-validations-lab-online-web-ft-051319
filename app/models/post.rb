f960120fbe40d6f259c90c0ac4a0c67e6bef8262
Kane Koo <kanekoo@gmail.com>
2019-07-24 16:41:17 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
+require 'pry'
+
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :clickbaity
+
+
+  def clickbaity
+    clickbaits=["Won\'t Believe", "Secret", "Top [number]", "Guess"]
+  #  binding.pry
+    # if title == nil
+    #   return false
+    # end
+    clickbait=clickbaits.find do |cb|
+      if title != nil
+        title.include?(cb)
+      end
+    end
+
+    unless clickbait
+      self.errors.add(:title, "must be clickbait-y")
+    end
+  end
 end
